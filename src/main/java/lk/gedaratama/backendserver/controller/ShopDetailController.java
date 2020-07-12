1de4f36ac9f9416477cd57014bb050417414e75d
TaniyaRodrigo <torodrigo1995@gmail.com>
2020-07-12 13:00:48 +0530

    new:Update Shop profile

--- src/main/java/lk/gedaratama/backendserver/controller/ShopDetailController.java
+++ src/main/java/lk/gedaratama/backendserver/controller/ShopDetailController.java
@@ -2,6 +2,9 @@ package lk.gedaratama.backendserver.controller;
 
 
 import lk.gedaratama.backendserver.model.ShopDetail;
+import lk.gedaratama.backendserver.repository.ShopDetailRepository;
+import lk.gedaratama.backendserver.resource.ShopDetailResource;
+import lk.gedaratama.backendserver.response.MessageResponse;
 import lk.gedaratama.backendserver.service.ShopDetailService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +20,30 @@ public class ShopDetailController {
         this.shopDetailService = shopDetailService;
     }
 
-    @GetMapping(value="/{view}")
+    @GetMapping(value="/{view/uuid}")
     public ResponseEntity<?> getShopDetailsByUuid(@PathVariable String uuid){
         ShopDetail shopDetail= shopDetailService.getShopDetailsByUuid(uuid);
         return ResponseEntity.ok(shopDetail);
     }
+
+    @PutMapping(value="/{update/uuid}")
+    public ResponseEntity<?> updateShopProfileDetail(@PathVariable("uuid") String uuid,  @RequestBody ShopDetail shopDetail) {
+        //shopDetailService.saveShopProfileDetailByUuid(uuid);
+        //ShopDetailService.saveShopProfileDetailByUuid(uuid);
+
+        ShopDetail updates = ShopDetailRepository.findById(uuid);
+        //.orElseThrow(() -> new ResourceNotFoundException("Shop not found for this id :: " + uuid));
+
+        updates.setShopName(shopDetail.getShopName());
+        updates.setShopOwnerName(shopDetail.getShopOwnerName());
+        updates.setMobileNo(shopDetail.getMobileNo());
+        updates.setLocation(shopDetail.getLocation());
+        updates.setEmail(shopDetail.getEmail());
+        updates.setNic(shopDetail.getNic());
+
+        return ResponseEntity.ok(new MessageResponse("Successfully Updated"));
+    }
+
 
 
 }
--- src/main/java/lk/gedaratama/backendserver/service/ShopDetailService.java
+++ src/main/java/lk/gedaratama/backendserver/service/ShopDetailService.java
@@ -22,4 +22,8 @@ public class ShopDetailService {
         return shopDetailRepository.findShopDtailByUserUuid(uuid);
     }
 
+
+
+
+
 }
